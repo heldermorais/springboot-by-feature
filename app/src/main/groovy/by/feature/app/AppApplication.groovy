321e0257331e1629ec4efeb787b6b1449104551a
Helder Morais <helder.morais@gmail.com>
2023-02-16 23:51:51 -0300

    Zerando .DS_Store...

---
+++ app/src/main/groovy/by/feature/app/AppApplication.groovy
@@ -0,0 +1,13 @@
+package by.feature.app
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AppApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(AppApplication, args)
+	}
+
+}
